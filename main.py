f973ce72b3b43ab2ede81bc93e1cfac847e69134
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2025-05-18 23:43:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/")
-def serve_index():
-    return FileResponse("static/index.html")
+@app.get("/main.html")
+def serve_main():
+    return FileResponse("static/main.html")
