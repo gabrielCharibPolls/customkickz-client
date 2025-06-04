4592514313900faea661d61a8ffea984a0c6ed04
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2025-06-04 22:50:54 +0100

    deug

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Serve le dossier static
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Accès direct à main.html via /
 @app.get("/")
 def root():
     return FileResponse("static/main.html")
