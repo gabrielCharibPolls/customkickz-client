5dc62f4900c5e16a665fcce45f8320a8f9408dba
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2025-06-05 12:53:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
-import os
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Serve le dossier static
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Accès direct à main.html via /
 @app.get("/")
-def read_root():
-    return {"message": "Hello from Railway"}
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+def root():
+    return FileResponse("static/main.html")
